e3c426a85beade3e1124afc329a78e54784dcd0c
datagreier2 <aron.lindegard@pm.me>
2026-01-31 00:09:53 +0100

    added directions, changed corner styling

--- src/components/Invitation.jsx
+++ src/components/Invitation.jsx
@@ -12,6 +12,55 @@ const Invitation = () => {
           Mer informasjon kommer.
         </p>
       </div>
+      <div className="location-card" data-animate="location">
+        <p className="location">
+          <a
+            href="https://www.olympen.no/oslo-nattklubb/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Festen vil være på <span className="underline">Pigalle</span>
+          </a>
+        </p>
+        <ul className="directions" data-animate="directions">
+          <li>
+            <a
+              href="https://maps.app.goo.gl/RXEv9XxYduyoQGoEA"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Fra Oslo S
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://maps.app.goo.gl/mcVQmqCnVF1BUymQ7"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Fra Grønland T
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://maps.app.goo.gl/euXgMbVPcnQqWapj7"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Fra 37-bussen
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://maps.app.goo.gl/b6yvfFBYxrg2fJxV8"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Fra P-hus
+            </a>
+          </li>
+        </ul>
+      </div>
     </main>
   )
 }
